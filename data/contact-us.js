b12abe8e1c1e3f352f71f218cff013ff7cb996f3
RohithReddy Gouni <grohithreddy9999@gmail.com>
2022-05-07 11:47:31 -0400

    Contact Us Page

---
+++ data/contact-us.js
@@ -0,0 +1,29 @@
+const mongoCollections = require("../config/mongoCollections");
+const contactusCollections = mongoCollections.contactus;
+const userCollections = mongoCollections.users;
+const validator = require("../helper/validator");
+const utils = require("../helper/utils");
+const errorCode = require("../helper/common").errorCode;
+const userData = require("../data/users");
+
+async function create(name, email, subject, message) {
+  // Input Validation by calling functions from validation.js
+  console.log("firstName", firstName);
+  const contactusData = {
+    name: name,
+    email: email,
+    subject: subject,
+    message: message,
+  };
+
+  const conatctusData = await contactusCollections();
+  const users = await userCollections();
+  const insertInfo = await conatctusData.insertOne(contactusData);
+
+  if (insertInfo.length === 0) throw new Error("Could not add a resume");
+  let id = insertInfo.insertedId;
+}
+
+module.exports = {
+  create,
+};
